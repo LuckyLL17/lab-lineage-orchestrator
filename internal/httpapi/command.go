435003b07405e4b000676735e6eb0b023a2114c7
LuckyLL17 <xiangyujia50@gmail.com>
2026-08-24 13:00:00 +0800

    red：含 Bug 加受保护测试验收快照

---
+++ internal/httpapi/command.go
@@ -0,0 +1,24 @@
+package httpapi
+
+import (
+	"net/http"
+
+	"github.com/local/lab-lineage-orchestrator/internal/app"
+)
+
+func (r *Router) command(w http.ResponseWriter, req *http.Request) {
+	var command app.Command
+	if req.Header.Get("X-Actor") != "" {
+		command.Actor = req.Header.Get("X-Actor")
+	}
+	if err := decodeJSON(req, &command); err != nil {
+		writeError(w, http.StatusBadRequest, err)
+		return
+	}
+	event, err := r.service.Apply(command)
+	if err != nil {
+		writeError(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	writeJSON(w, http.StatusAccepted, event)
+}
